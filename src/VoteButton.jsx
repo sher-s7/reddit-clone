e62b31f79d1a1fb0b2a687da542442d29d814dbb
Sher <sher.sheikh77@gmail.com>
2020-08-16 14:11:17 -0400

    Fix React state memory leak problem

--- src/VoteButton.jsx
+++ src/VoteButton.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import fire from './config/Fire';
 import firebase from 'firebase/app';
 export default class VoteButton extends React.Component {
+    _isMounted = false;
     constructor(props) {
         super(props);
         this.state = {
@@ -10,15 +11,17 @@ export default class VoteButton extends React.Component {
     }
 
     componentDidMount() {
+        this._isMounted = true;
         const user = fire.auth().currentUser;
         fire.firestore().collection(this.props.collection).doc(this.props.doc.id).get().then(docRef => {
-            
-            this.setState({ doc: docRef, points: docRef.data().points, currentUser: user })
-            if (user) {
-                if (docRef.data().votes[user.uid] === 1) {
-                    this.setState({ vote: 'upvoted' })
-                } else if (docRef.data().votes[user.uid] === -1) {
-                    this.setState({ vote: 'downvoted' })
+            if (this._isMounted) {
+                this.setState({ doc: docRef, points: docRef.data().points, currentUser: user })
+                if (user) {
+                    if (docRef.data().votes[user.uid] === 1 && this._isMounted) {
+                        this.setState({ vote: 'upvoted' })
+                    } else if (docRef.data().votes[user.uid] === -1 && this._isMounted) {
+                        this.setState({ vote: 'downvoted' })
+                    }
                 }
             }
         });
@@ -30,10 +33,17 @@ export default class VoteButton extends React.Component {
         }
     }
 
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
 
     updateDoc = () => {
+
         fire.firestore().collection(this.props.collection).doc(this.props.doc.id).get().then(docRef => {
-            this.setState({ doc: docRef })
+            if (this._isMounted) {
+                this.setState({ doc: docRef })
+            }
         });
     }
 
@@ -44,37 +54,37 @@ export default class VoteButton extends React.Component {
             const votesMap = doc.data().votes;
             const uid = currentUser.uid;
             if (votesMap[uid] === 1) {
-                
+
                 fire.firestore().collection(this.props.collection).doc(this.props.doc.id).set({
                     votes: {
                         [uid]: 0
                     },
                     points: firebase.firestore.FieldValue.increment(-1)
-                }, {merge: true});
+                }, { merge: true });
                 this.setState((prevState) => ({
                     vote: '',
                     points: prevState.points - 1,
                 }));
             } else if (votesMap[uid] === -1) {
-                
+
                 fire.firestore().collection(this.props.collection).doc(this.props.doc.id).set({
                     votes: {
                         [uid]: 1
                     },
                     points: firebase.firestore.FieldValue.increment(2)
-                }, {merge: true});
+                }, { merge: true });
                 this.setState((prevState) => ({
                     vote: 'upvoted',
                     points: prevState.points + 2,
                 }));
             } else {
-                
+
                 fire.firestore().collection(this.props.collection).doc(this.props.doc.id).set({
                     votes: {
                         [uid]: 1
                     },
                     points: firebase.firestore.FieldValue.increment(1)
-                }, {merge: true});
+                }, { merge: true });
                 this.setState((prevState) => ({
                     vote: 'upvoted',
                     points: prevState.points + 1,
@@ -94,37 +104,37 @@ export default class VoteButton extends React.Component {
             const votesMap = doc.data().votes;
             const uid = currentUser.uid;
             if (votesMap[uid] === 1) {
-                
+
                 fire.firestore().collection(this.props.collection).doc(this.props.doc.id).set({
                     votes: {
                         [uid]: -1
                     },
                     points: firebase.firestore.FieldValue.increment(-2)
-                }, {merge: true});
+                }, { merge: true });
                 this.setState((prevState) => ({
                     vote: 'downvoted',
                     points: prevState.points - 2,
                 }));
             } else if (votesMap[uid] === -1) {
-                
+
                 fire.firestore().collection(this.props.collection).doc(this.props.doc.id).set({
                     votes: {
                         [uid]: 0
                     },
                     points: firebase.firestore.FieldValue.increment(1)
-                }, {merge: true});
+                }, { merge: true });
                 this.setState((prevState) => ({
                     vote: '',
                     points: prevState.points + 1,
                 }));
             } else {
-                
+
                 fire.firestore().collection(this.props.collection).doc(this.props.doc.id).set({
                     votes: {
                         [uid]: -1
                     },
                     points: firebase.firestore.FieldValue.increment(-1)
-                }, {merge: true});
+                }, { merge: true });
                 this.setState((prevState) => ({
                     vote: 'downvoted',
                     points: prevState.points - 1,
